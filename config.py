fe84cbb91577f3fafddcecd854de196c941d71bf
Muamen-Elnagar <101446579+Elnagar0@users.noreply.github.com>
2022-10-04 04:14:20 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,14 +3,14 @@ from os import environ
 
 load_dotenv("config.env")
 
-BOT_TOKEN = environ.get("BOT_TOKEN", "5052091619:AAG--am0NiA5TMwyYYu2QxsdGTEwGUPCI_w")
-API_ID = int(environ.get("API_ID", "11100579"))
-API_HASH = environ.get("API_HASH", "ebe36eff0de946519c0af0ffe0517b5e")
+BOT_TOKEN = environ.get("BOT_TOKEN")
+API_ID = int(environ.get("API_ID"))
+API_HASH = environ.get("API_HASH")
 SUDO_USERS_ID = environ.get("SUDO_USERS_ID", "1970797144")
 LOG_GROUP_ID = environ.get("LOG_GROUP_ID", "-1001569455288")
-MONGO_URL = environ.get("MONGO_URL", "mongodb+srv://vampir:Al2552001@cluster0.yeaoc.mongodb.net/vampir?retryWrites=true&w=majority")
+MONGO_URL = environ.get("MONGO_URL")
 BASE_DB = MONGO_URL
 ARQ_API_URL = environ.get("ARQ_API_URL", "https://arq.hamker.in")
-ARQ_API_KEY = environ.get("ARQ_API_KEY", "PLBNYD-CGIRFG-RECXWF-UFZIOA-ARQ")
+ARQ_API_KEY = environ.get("ARQ_API_KEY")
 F_SUB_CHANNEL = environ.get("F_SUB_CHANNEL")
 OWNER_ID = int(environ.get("OWNER_ID","1970797144"))
